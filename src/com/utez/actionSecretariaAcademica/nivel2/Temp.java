a2b319e4372be7c23979651afa69e964191e31e3
xenonDevelops <xenondevelops@gmail.com>
2014-07-22 13:28:16 -0500

    Creacion de paquetes para la migración de la base de datos Secretaria Academica

---
+++ src/com/utez/actionSecretariaAcademica/nivel2/Temp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utez.actionSecretariaAcademica.nivel2;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Temp {
+    
+}
---
+++ src/com/utez/actionSecretariaAcademica/nivel5/Temp.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.utez.actionSecretariaAcademica.nivel5;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Temp {
+    
+}
